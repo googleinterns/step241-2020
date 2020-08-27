a684d3b6bc18d2077243ad379e31bde97475eadb
Rania Rusly <rania.rusly@gmail.com>
2020-08-27 18:18:09 +0000

    create class to represent user details

---
+++ src/main/java/com/google/sps/data/UserData.java
@@ -0,0 +1,35 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.data;
+
+/** The class represents user details. */
+public final class Comment {
+
+  private final String userEmail;
+  private final String name;
+  private final String department;
+  private final String bio;
+  private final String profilePicUrl;
+  private final long timestamp;
+
+  public Comment(String userEmail, String name, String department, String bio, String profilePicUrl, long timestamp) {
+    this.userEmail = userEmail;
+    this.name = name;
+    this.department = department;
+    this.bio = bio;
+    this.profilePicUrl = profilePicUrl;
+    this.timestamp = timestamp;
+  }
+}
