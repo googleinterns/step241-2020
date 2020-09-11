3357febceef280614f883e2867108c9be6e1e16b
Lorna Armstrong <s1812323@ed.ac.uk>
2020-09-11 15:59:40 +0000

    Add HTML for top 5 recommendations

--- src/main/java/com/google/sps/servlets/RecommenderServlet.java
+++ src/main/java/com/google/sps/servlets/RecommenderServlet.java
@@ -32,7 +32,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 @WebServlet("/recommender")
 public class RecommenderServlet extends HttpServlet {
 
